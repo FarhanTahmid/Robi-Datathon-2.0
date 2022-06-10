4a800dc967bdb03fb51458563074a8f4c98a6d38
Farhan Tahmid <farhantahmid881@gmail.com>
2022-06-10 22:46:26 +0600

    submission2

--- neural network.py
+++ neural network.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 dataset = pd.read_csv('train.csv')
-dataset.drop(dataset.columns[[13, 14, 16,17,18,19,10,14,15,12]], axis = 1, inplace = True)
+dataset.drop(dataset.columns[[0,13, 14, 16,17,18,19,10,11,14,15,12]], axis = 1, inplace = True)
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
@@ -20,43 +20,42 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 le = LabelEncoder()
 
-#ID
-X[:,0]=le.fit_transform(X[:,0])
 #gender
-X[:,1]=le.fit_transform(X[:,1])
+X[:,0]=le.fit_transform(X[:,0])
 #s11
-X[:,2]=le.fit_transform(X[:,2])
+X[:,1]=le.fit_transform(X[:,1])
 #s12
-X[:,3]=le.fit_transform(X[:,3])
+X[:,2]=le.fit_transform(X[:,2])
 #s16
-X[:,5]=le.fit_transform(X[:,5])
+X[:,3]=le.fit_transform(X[:,3])
 #s17
-X[:,6]=le.fit_transform(X[:,6])
+X[:,4]=le.fit_transform(X[:,4])
+X[:,5]=le.fit_transform(X[:,5])
 #s18
-X[:,7]=le.fit_transform(X[:,7])
+X[:,6]=le.fit_transform(X[:,6])
 
 
 X=pd.DataFrame(X)
 
 #processing s52
-X[9].replace(to_replace = 'l', value =float(1),inplace=True)
-X[9].replace(to_replace = 'o', value =float(0),inplace=True)
-X[9]=X[9].astype(np.int32)
-X[9].unique()
+X[8].replace(to_replace = 'l', value =float(1),inplace=True)
+X[8].replace(to_replace = 'o', value =float(0),inplace=True)
+X[8]=X[8].astype(np.float64)
+X[8].unique()
 
 #extracting numerical values from s54
-X[10].replace(to_replace = 'b2', value ='0',inplace=True)
-X[10].replace(to_replace = '2b', value ='0',inplace=True)
-X[10].replace(to_replace = 'bb', value ='0',inplace=True)
-X[10].replace(to_replace = 'aa', value ='1',inplace=True)
-X[10].replace(to_replace = 'a2', value ='1',inplace=True)
-X[10].replace(to_replace = '2a', value ='1',inplace=True)
-X[10].replace(to_replace = 'ab', value ='3',inplace=True)
-X[10].replace(to_replace = 'ba', value ='3',inplace=True)
-X[10].replace(to_replace = '22', value ='2',inplace=True)
+#X[10].replace(to_replace = 'b2', value ='0',inplace=True)
+#X[10].replace(to_replace = '2b', value ='0',inplace=True)
+#X[10].replace(to_replace = 'bb', value ='0',inplace=True)
+#X[10].replace(to_replace = 'aa', value ='1',inplace=True)
+#X[10].replace(to_replace = 'a2', value ='1',inplace=True)
+#X[10].replace(to_replace = '2a', value ='1',inplace=True)
+#X[10].replace(to_replace = 'ab', value ='3',inplace=True)
+#X[10].replace(to_replace = 'ba', value ='3',inplace=True)
+#X[10].replace(to_replace = '22', value ='2',inplace=True)
 
-X[10]=X[10].fillna(0)  #try another model excluding the missing values  
-X[10] = X[10].astype(np.int32)
+#X[10]=X[10].fillna(0)  #try another model excluding the missing values  
+#X[10] = X[10].astype(np.int32)
 
 #Extracting numerical values from s55
 #X[11].replace(to_replace = 'kK', value ='0',inplace=True)
@@ -101,49 +100,50 @@ cm = confusion_matrix(yTest, y_pred)
 print(cm)
 accuracy_score(yTest, y_pred)*100
 
+
+###### TEST DATASET WORK
+
 testData=pd.read_csv('test.csv')
-testData.drop(testData.columns[[13, 14, 16,17,18,19,10,14,15]], axis = 1, inplace = True)
-testDataX = testData.iloc[:, :].values
+testData.drop(testData.columns[[0,13, 14, 16,17,18,19,10,11,14,15,12]], axis = 1, inplace = True)
+X = testData.iloc[:, :].values
 
 #gender
-testDataX[:, 0] = le.fit_transform(testDataX[:, 2])
+X[:,0]=le.fit_transform(X[:,0])
+#gender
+X[:,1]=le.fit_transform(X[:,1])
 #s11
-testDataX[:, 1] = le.fit_transform(testDataX[:, 2])
+X[:,2]=le.fit_transform(X[:,2])
 #s12
-testDataX[:, 2] = le.fit_transform(testDataX[:, 2])
-#s58
-testDataX[:, 5] = le.fit_transform(testDataX[:, 2])
+X[:,3]=le.fit_transform(X[:,3])
+#s16
+X[:,4]=le.fit_transform(X[:,4])
+#s17
+X[:,5]=le.fit_transform(X[:,5])
+#s18
+X[:,6]=le.fit_transform(X[:,6])
 
-testDataX=pd.DataFrame(testDataX)
-testDataX[9].replace(to_replace = 'l', value ='1',inplace=True)
-testDataX[9].replace(to_replace = 'o', value ='0',inplace=True)
+X=pd.DataFrame(X)
+X[8].replace(to_replace = 'l', value ='1',inplace=True)
+X[8].replace(to_replace = 'o', value ='0',inplace=True)
+X[8]=X[8].astype(np.float64)
+X[8].unique()
 
-#extracting numerical values from s54
-testDataX[10].replace(to_replace = 'b2', value ='0',inplace=True)
-testDataX[10].replace(to_replace = '2b', value ='0',inplace=True)
-testDataX[10].replace(to_replace = 'bb', value ='0',inplace=True)
-testDataX[10].replace(to_replace = 'aa', value ='1',inplace=True)
-testDataX[10].replace(to_replace = 'a2', value ='1',inplace=True)
-testDataX[10].replace(to_replace = '2a', value ='1',inplace=True)
-testDataX[10].replace(to_replace = 'ab', value ='3',inplace=True)
-testDataX[10].replace(to_replace = 'ba', value ='3',inplace=True)
-testDataX[10].replace(to_replace = '22', value ='2',inplace=True)
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
+y_pred = ann.predict(X)
 
+for cell in np.nditer(y_pred, op_flags=['readwrite']):
+    if cell[...] > 0.5:
+      cell[...] = 1
+    else:
+      cell[...] = 0
 
-testResults=[]
-testDataX['label']=testResults
-testDataY=testDataX.iloc[:, -1].values
-testDataX = sc.fit_transform(testDataX)
-testDataY = sc.fit_transform(testDataY)
-for column in testDataX:
-    y_pred = ann.predict(testDataX)
-    y_pred = (y_pred > 0.5)
-    np.concatenate((y_pred.reshape(len(y_pred), 1), testDataY.reshape(len(yTest), 1)), 1)
-    testResults.append(y_pred)
+test2_dataset = pd.read_csv('test.csv')
+test2_dataset['label'] = y_pred
+submission = test2_dataset[["id", "label"]]
 
-
-
-
+submission.to_csv("submission2.csv", index = False)
 
 
 
